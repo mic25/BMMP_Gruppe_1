543194a3b9121ffef0abe23787c47b723a5f7431
sMeista <katharina.sachi@hotmail.de>
2013-10-01 13:14:13 +0200

    char game index preload

--- Goblin Race/game.js
+++ Goblin Race/game.js
@@ -5,13 +5,14 @@
 
 	this.bg = new createjs.Bitmap(queue.getResult("bg"));
 	this.ground = new createjs.Bitmap(queue.getResult("ground"));
+    this.c = new Char(); 
 }
 
 Game.prototype.handleTick = function () {
 
     this.bg.x += this.bg_speed1;
 	this.ground.x += this.ground_speed;
-
+   
 
     /*if (Key.isDown(Key.UP)) {
         char.jump();
@@ -32,6 +33,7 @@ Game.prototype.start = function(){
 
 	stage.addChild(this.bg);
 	stage.addChild(this.ground);
+    stage.addChild(this.c.image);
 	stage.update();
 
 }
--- Goblin Race/preload.js
+++ Goblin Race/preload.js
@@ -2,7 +2,7 @@
     var manifest = [
         {id:"bg", src:"img/Himmel.png"},
         {id:"ground", src:"img/Wiese.png"},
-        {id:"figure", scr:"img/schneemann"}
+        {id:"figure", src:"img/schneemann.png"}
     ];
     queue = new createjs.LoadQueue(false);
     queue.installPlugin(createjs.Sound);
