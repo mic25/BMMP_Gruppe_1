32f1f44eae52ccd1329bb5d8b65c1d37470bcb15
Exodem <Exodem@gmx.de>
2013-10-01 10:49:42 +0200

    Menu

--- Goblin Race/menu.js
+++ Goblin Race/menu.js
@@ -1,5 +1,20 @@
-﻿function Menu() {
-
-    //start new game
+﻿function Menu() {    
 
 }
+
+Menu.prototype.showMenu = function () {
+
+    // show the Menu
+
+    //For test purposes
+    this.startGame();
+}
+
+Menu.prototype.startGame = function () {
+
+    game = new Game();
+    controller = new COntroller();
+    game.start();
+
+}
+
