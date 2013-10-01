be02b0b82426d9c3e09d5d8a89e96ddf9c19cc3c
mic25 <mirisweety@dhcp-138-246-112-156.dynamic.eduroam.mwn.de>
2013-10-01 13:17:56 +0200

    Merge branch 'master' of https://github.com/mic25/BMMP_Gruppe_1
    Conflicts:
    	Goblin Race/game.js

--- Goblin Race/char.js
+++ Goblin Race/char.js
@@ -1,3 +1,13 @@
-﻿function Char() {
+function Char() {
+//Startposition
+var startX = 50;
+var startY = 700;
+
+//Grafik laden
+this.image = new createjs.Bitmap(queue.getResult("figure"));
+this.image.regX = 30;
+this.image.rexY = 30;
+this.image.x = startX;
+this.image.y = startY;
 
 }
--- Goblin Race/game.js
+++ Goblin Race/game.js
@@ -5,13 +5,26 @@
 
 	this.bg = new createjs.Bitmap(queue.getResult("bg"));
 	this.ground = new createjs.Bitmap(queue.getResult("ground"));
+    this.c = new Char(); 
 }
 
 Game.prototype.handleTick = function () {
 
-    this.bg.x += this.bg_speed1;
+	this.bg.x += this.bg_speed1;
 	this.ground.x += this.ground_speed;
+<<<<<<< HEAD
 
+	if(this.bg.x+this.bg.image.width < stage.canvas.width){
+		this.bg.x = 0;
+	}
+
+	if(this.ground.x+this.ground.image.width < stage.canvas.width){
+		this.ground.x = 0;
+	}
+    
+=======
+   
+>>>>>>> 543194a3b9121ffef0abe23787c47b723a5f7431
 
     /*if (Key.isDown(Key.UP)) {
         char.jump();
@@ -32,6 +45,7 @@ Game.prototype.start = function(){
 
 	stage.addChild(this.bg);
 	stage.addChild(this.ground);
+    stage.addChild(this.c.image);
 	stage.update();
 
 }
