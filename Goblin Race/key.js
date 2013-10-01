df5bba1b8901bc9cea70bdba6b1fe9ca3af1e4a2
mic25 <mirisweety@dhcp-138-246-112-156.dynamic.eduroam.mwn.de>
2013-10-01 10:14:49 +0200

    key-script

---
+++ Goblin Race/key.js
@@ -0,0 +1,26 @@
+var Key = {
+  	_pressed: {},
+
+ 	LEFT: 37,
+  	UP: 38,
+  	RIGHT: 39,
+  	DOWN: 40,
+  	SPACE:32,
+  
+	  isDown: function(keyCode) {
+	    return this._pressed[keyCode];
+	  },
+  
+	  onKeydown: function(event) {
+	  	//console.log(event.keyCode);
+	    this._pressed[event.keyCode] = true;
+	  },
+  
+	  onKeyup: function(event) {
+	    delete this._pressed[event.keyCode];
+	  },
+
+	  isEmpty: function() {
+	  	return Object.keys(this._pressed).length>0;
+	  }
+};
