c5571770e491037c8925f052ecda8672e1528158
sMeista <katharina.sachi@hotmail.de>
2013-10-01 10:52:06 +0200

    beab

--- Goblin Race/preload.js
+++ Goblin Race/preload.js
@@ -2,6 +2,7 @@
     var manifest = [
         {id:"bg", src:"img/Himmel.png"},
         {id:"ground", src:"img/Wiese.png"},
+        {id:"figure", scr:"img/schneemann"}
     ];
     queue = new createjs.LoadQueue(false);
     queue.installPlugin(createjs.Sound);
